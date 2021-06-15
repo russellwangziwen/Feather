3e07554d7729145b6426b64e25178b370709f5bb
pastFeather <66552572+pastFeather@users.noreply.github.com>
2021-06-15 11:13:38 +0800

    Update gittest.go

--- gittest.go
+++ gittest.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	nums := []int{1, 3, 5, 7}
-	nums = append(nums, 9)
+	nums := []int{1, 3, 5, 7, 9}
+	nums = append(nums, 11)
 	for _, v := range nums {
 		fmt.Println(v)
 	}
