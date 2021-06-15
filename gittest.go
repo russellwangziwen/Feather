e04a3174bd3cfa6954e3e0bb7766629ac3c4765a
russellwangziwen <russellwangziwen@didiglobal.com>
2021-06-15 14:26:47 +0800

    test

--- gittest.go
+++ gittest.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	nums := []int{1, 3, 5, 7, 9, 11, 13}
-	nums = append(nums, 15)
+	nums := []int{1, 3, 5, 7, 9, 11, 13, 15}
+	nums = append(nums, 17)
 	for _, v := range nums {
 		fmt.Println(v)
 	}
