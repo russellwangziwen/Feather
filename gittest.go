ed4f3fc1c32e124f08635d6fd374652e42855fc1
russellwangziwen <russellwangziwen@didiglobal.com>
2021-06-16 15:55:59 +0800

    add

--- gittest.go
+++ gittest.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	nums := []int{1, 3, 5, 7, 9, 11, 13, 15}   //打印奇数数组
+	nums := []int{3, 3, 5, 7, 9, 11, 13, 15}   //打印奇数数组
 	nums = append(nums, 19)
 	for _, v := range nums {
 		fmt.Println(v)
